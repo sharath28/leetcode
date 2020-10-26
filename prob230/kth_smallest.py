21f1c30c08efdd7be2fca6b7013bc18973b19b0c
sharath28 <sharath0628@gmail.com>
2020-10-26 19:35:35 -0400

    Kth Smallest node in the BST

---
+++ prob230/kth_smallest.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+###############
+#Inorder Traversal
+#         def inorder(r):
+#             return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+
+#         return inorder(root)[k-1]
+
+###############
+#Stack
+        stack = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
