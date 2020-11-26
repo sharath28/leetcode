4cc48fcfd1eebcb663466cd44718305cb02cbb72
sharath28 <sharath0628@gmail.com>
2020-11-25 23:42:24 -0500

    Valid Word Squares

---
+++ prob422/valid_word_square.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def validWordSquare(self, words):
+        """
+        :type words: List[str]
+        :rtype: bool
+        """
+        for i in range(len(words)):
+            for j in range(len(words[i])):
+                try:
+                    if words[i][j] != words[j][i]:
+                        return False
+                except IndexError:
+                    return False
+        return True
