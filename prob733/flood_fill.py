6cc6bc6a15586415c29abbad0b8aadcd0b7e61b8
sharath28 <sharath0628@gmail.com>
2020-11-26 11:45:33 -0500

    Flood Fill

---
+++ prob733/flood_fill.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        row, col = len(image), len(image[0])
+        color = image[sr][sc]
+        if color == newColor:
+            return image
+
+        def dfs(r,c):
+            if image[r][c] == color:
+                image[r][c] = newColor
+                if r >= 1:
+                    dfs(r-1,c)
+                if r+1 < row:
+                    dfs(r+1,c)
+                if c >= 1:
+                    dfs(r,c-1)
+                if c+1 < col:
+                    dfs(r,c+1)
+
+        dfs(sr,sc)
+        return image
