c881f6a82fce59827a742977fc207c7bf65518c3
sharath28 <sharath0628@gmail.com>
2020-11-25 19:41:09 -0500

    Integer To Roman

---
+++ prob12/integer_to_roman.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        d = { "I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        digits = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+        roman_digits = []
+        for value,symbol in digits:
+            if num ==0: break
+            count , num = divmod(num,value)
+            roman_digits.append(symbol*count)
+        return "".join(roman_digits)
