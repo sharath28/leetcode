1deeda247fd90b38e478b1a2e6ad167f0be2cca7
sharath28 <sharath0628@gmail.com>
2020-12-15 16:30:30 -0500

    Is Subsequence

---
+++ prob392/Is_subsequence.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        source_len, target_len = len(s), len(t)
+        if source_len == 0:
+            return True
+
+        dp = [[0]*(target_len+1) for _ in range(source_len+1)]
+
+        for col in range(1,target_len+1):
+            for row in range(1,source_len+1):
+                if s[row-1] == t[col-1]:
+                    dp[row][col] = dp[row-1][col-1]+1
+                else:
+                    dp[row][col] = max(dp[row][col-1],dp[row-1][col])
+
+            if dp[source_len][col] == source_len:
+                return True
+        return False
