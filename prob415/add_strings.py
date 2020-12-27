c01db5a3fd034985ee3c68ea1cb80eb32ab7b6be
sharath28 <sharath0628@gmail.com>
2020-12-27 09:36:18 -0500

    Add Strings

---
+++ prob415/add_strings.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        # return str(int(num1)+int(num2))
+        res = []
+
+        carry = 0
+        p1 = len(num1) - 1
+        p2 = len(num2) - 1
+        while p1 >= 0 or p2 >= 0:
+            x1 = ord(num1[p1]) - ord('0') if p1 >= 0 else 0
+            x2 = ord(num2[p2]) - ord('0') if p2 >= 0 else 0
+            value = (x1 + x2 + carry) % 10
+            carry = (x1 + x2 + carry) // 10
+            res.append(value)
+            p1 -= 1
+            p2 -= 1
+
+        if carry:
+            res.append(carry)
+
+        return ''.join(str(x) for x in res[::-1])
+
+        
