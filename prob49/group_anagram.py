e12f45c1c7f5eeb4664c72e07f69ef12d9a65788
sharath28 <sharath0628@gmail.com>
2021-01-06 11:03:56 -0500

    Group Anagram

---
+++ prob49/group_anagram.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
