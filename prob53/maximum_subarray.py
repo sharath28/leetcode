ef7d129a51848b8062d13bd18076c159df84b498
sharath28 <sharath0628@gmail.com>
2021-01-22 22:55:17 -0500

    Maximum Subarray

---
+++ prob53/maximum_subarray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        max_sum = nums[0]
+        for i in range(1,n):
+            if nums[i-1]>0:
+                nums[i] += nums[i-1]
+            max_sum = max(nums[i],max_sum)
+            print(max_sum)
+        return max_sum
